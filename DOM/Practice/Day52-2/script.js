f816e4e16ad896bd4b5b4a0363e8d8ef60ab4615
Kush Gohil <gohilkush761@gmail.com>
2025-12-12 10:39:54 +0530

    completed Matrix text effect using DOM Manipulation

--- DOM/Practice/Day52-2/script.js
+++ DOM/Practice/Day52-2/script.js
@@ -1,12 +1,22 @@
 const para = document.querySelector('p');
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const text = para.innerText;
+let iteration = 0;
 
 para.addEventListener('mouseenter', ()=>{
-    setInterval(()=>{
+    let effect = setInterval(()=>{
         const str = text.split('').map((char, index)=>{
+            if(index<iteration){
+                return char;
+            }
             return characters[Math.floor(Math.random() * characters.length)];
         }).join('');
         para.innerText = str;
-    }, 100)
+        iteration += 0.4;
+    }, 30)
+    setTimeout(()=>{
+        clearInterval(effect);
+        para.innerText = text;
+        iteration = 0;
+    }, 3000);
 })
