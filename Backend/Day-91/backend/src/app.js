d12b2b72da6842c7979f8520d16b7c2adeafb279
Kush Gohil <gohilkush761@gmail.com>
2026-02-11 22:22:06 +0530

    Day-94 work done in Day-91 directory

--- Backend/Day-91/backend/src/app.js
+++ Backend/Day-91/backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const noteModel = require("./model/notes.model");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 app.use(cors())
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "/public")));
 
 app.post("/api/notes", async (req, res) => {
   const { title, description } = req.body;
@@ -42,4 +45,7 @@ app.patch("/api/notes/:id", async (req, res)=>{
   })
 })
 
+app.use('*name', (req, res)=>{
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+})
 module.exports = app;
--- Backend/Day-91/frontend/src/App.jsx
+++ Backend/Day-91/frontend/src/App.jsx
@@ -1,40 +1,87 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react';
-const App = () => {
-  const [notes, setnotes] = useState([
-    {
-      title: "test title 1",
-      description: "test description 1"
-    },
-    {
-      title: "test title 2",
-      description: "test description 2"
-    },
-    {
-      title: "test title 3",
-      description: "test description 3"
-    },
-  ]);
-  useEffect(()=>{
-    axios.get("http://localhost:3000/api/notes")
-    .then((res)=>{
-      setnotes(res.data.notes)
-    })
-  }, [])
-  return (
-    <div className='notes'>
-      {
-        notes.map(note => {
-          return <div className='note'>
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
-          </div>
-        })
-      }
-    </div>
-  )
-}
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import {CirclePlus, CircleX, Pencil} from "lucide-react";
 
-export default App
+const App = () => {
+  const [notes, setnotes] = useState([]);
+  function fetchNotes(){
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setnotes(res.data.notes);
+    });
+  }
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  function handleSubmit(e){
+    e.preventDefault();
+    const {title, description} = e.target;
+    if(title.value === '' || description.value === ''){
+      return alert('Title or Description is empty.');
+    }
+    axios.post("http://localhost:3000/api/notes",{
+      title: title.value,
+      description: description.value
+    })
+    .then((res)=>{
+      // console.log(res);
+      fetchNotes();
+    })
+    title.value = '';
+    description.value = '';
+  }
+  function handleDelete(noteId){
+    const wantTo = confirm("Are you want to delete this note");
+    if(wantTo){
+      axios.delete(`http://localhost:3000/api/notes/${noteId}`)
+      .then((res)=>{
+        fetchNotes();
+      })
+    }else{
+      return ;
+    }
+  }
+  function handleEdit(noteId){
+    const newDesc = prompt("Enter new description : ");
+    console.log(newDesc, noteId);
+    if(newDesc  || newDesc.trim() !== ''){
+      axios.patch(`http://localhost:3000/api/notes/${noteId}`, {
+        description: newDesc
+      })
+      .then((res)=>{
+        console.log(res.data);
+        fetchNotes();
+      })
+    }else{
+      return ;
+    }
+  }
+  return (
+    <>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Enter note title"/>
+        <input type="text" name="description" placeholder="Enter note description"/>
+        <button>Create note <CirclePlus/></button>
+      </form>
+      <div className="notes">
+        {notes.map((note, idx) => {
+          return (
+            <div className="note" id={idx}>
+              <div className="inner">
+                <h1>{note.title}</h1>
+                <p>{note.description}</p>
+              </div>
+              <div className="btns">
+                <button className="delete-btn" onClick={()=>handleDelete(note._id)}><CircleX/></button>
+                <button className="edit-btn" onClick={()=>handleEdit(note._id)}><Pencil/></button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default App;
