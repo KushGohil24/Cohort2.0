8f0d8206b931b1c0da7e239c599d56278a5690fe
Kush Gohil <gohilkush761@gmail.com>
2026-02-24 08:46:38 +0530

    fixed unauthorised and not found error as await is not there

--- Backend/authentication/src/routes/auth.routes.js
+++ Backend/authentication/src/routes/auth.routes.js
@@ -46,7 +46,7 @@ authRouter.get("/get-me", async (req, res) => {
 
 authRouter.post("/login", async (req, res) => {
     const {email, password} = req.body;
-    const user = userModel.findOne({email});
+    const user = await userModel.findOne({email});
     if(!user){
         return res.status(404).json({
             message: "User not found"
