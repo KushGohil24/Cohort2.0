adc7d9e8a47ab28ae36a7d2305fce1d79294976f
Kush Gohil <gohilkush761@gmail.com>
2026-02-25 16:18:09 +0530

    Day-101 Backend Project-1 InstaClone

---
+++ Project/Backend/InstaClone/src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { registerController, loginController } = require("../controllers/auth.controller");
+const authRouter = express.Router();
+
+authRouter.post("/register", registerController);
+
+authRouter.post("/login", loginController);
+module.exports = authRouter;
